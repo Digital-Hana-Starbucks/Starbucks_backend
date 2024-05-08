add06ae52990c946207974ea9bdf4a5ad85ae999
mummhy0811 <mummhy0811@gmail.com>
2024-05-08 13:35:52 +0900

    Feat: 회원 적립금 조회(#38)
    close: #38

---
+++ src/main/java/com/hanaro/starbucks/dto/member/PointResDto.java
@@ -0,0 +1,10 @@
+package com.hanaro.starbucks.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PointResDto {
+    private int point;
+}
--- src/main/java/com/hanaro/starbucks/repository/MemberRepository.java
+++ src/main/java/com/hanaro/starbucks/repository/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.hanaro.starbucks.repository;
 
+import com.hanaro.starbucks.dto.member.PointResDto;
 import com.hanaro.starbucks.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     Optional<Member> findByUserId(String userId);
     boolean existsByUserId(String userId);
+
+    @Query("Select new com.hanaro.starbucks.dto.member.PointResDto(m.userPoint) from Member m where m.userIdx = :userIdx")
+    PointResDto findPointByUserIdx(int userIdx);
 }
